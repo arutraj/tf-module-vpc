40b3bfb2cdb29e9dfbc232987007324b0291380d
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-28 08:01:09 +0530

    Expense Project Terraform

---
+++ locals.tf
@@ -0,0 +1,16 @@
+locals {
+
+  tags = merge(var.tags,
+    {
+      module_name = "tf-module-vpc"
+    }
+  )
+
+  vpc_tags = merge(local.tags,
+    {
+      Name = "${var.env}-vpc"
+    }
+  )
+
+}
+
--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "env" {}
 variable "vpc_cidr_block" {}
-
-
+variable "web_subnets_cidr" {}
+variable "app_subnets_cidr" {}
+variable "db_subnets_cidr" {}
+variable "tags" {}
 
