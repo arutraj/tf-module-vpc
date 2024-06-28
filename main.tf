3a992fc6d39f72197ceb9bfa7651bb994ee7a2e5
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-28 08:17:29 +0530

    Expense Project Terraform

--- main.tf
+++ main.tf
@@ -31,5 +31,6 @@ resource "aws_vpc_peering_connection" "main" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
+  auto_accept   = true
 }
 
