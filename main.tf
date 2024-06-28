8445e52ead83e3ce890e1b6cba3790b7cc346e28
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-28 08:21:05 +0530

    Expense Project Terraform

--- main.tf
+++ main.tf
@@ -34,3 +34,15 @@ resource "aws_vpc_peering_connection" "main" {
   auto_accept   = true
 }
 
+resource "aws_route" "main-vpc" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = var.default_vpc_rt
+  destination_cidr_block    = var.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+
+}
