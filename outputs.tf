670b2728764b9ffea8858d243b11222c09f71da9
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-02 08:07:39 +0530

    Expense Project Terraform

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "lb_subnet_ids" {
+  value = aws_subnet.lb.*.id
+}
+
+output "eks_subnet_ids" {
+  value = aws_subnet.eks.*.id
+}
+
+output "db_subnet_ids" {
+  value = aws_subnet.db.*.id
+}
+
+
